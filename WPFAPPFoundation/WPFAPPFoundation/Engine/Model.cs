8a74ddc9b0c2062b9a75b19a7625e418c1447a54
GodOfAxolotl <70233758+GodOfAxolotl@users.noreply.github.com>
2021-06-15 22:02:16 +0200

    Add files via upload

---
+++ WPFAPPFoundation/WPFAPPFoundation/Engine/Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAPPFoundation.Engine
+{
+    class Model
+    {
+
+        //Modelsample 
+
+        private int _x;
+        public int x
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public Model(int x)
+        {
+            this.x = x;
+        }
+
+        public void update()
+        {
+            //TODO: Add needed update to this Method
+        }
+    }
+}
---
+++ WPFAPPFoundation/WPFAPPFoundation/Session.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAPPFoundation
+{
+    public class Session : BaseNotificationClass
+    {
+        private int _x;
+        public int x
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public Session()
+        {
+
+        }
+
+        public void loop()
+        {
+
+        }
+
+        private void update()
+        {
+            OnPropertyChanged(nameof(x));
+        }
+    }
+}
